026892c009b16256724132769017ad91eee91965
Ben Veenema <bveenema@gmail.com>
2019-06-27 08:49:59 -0400

    Update

--- src/MM_Object_Base.h
+++ src/MM_Object_Base.h
@@ -2,20 +2,25 @@
 #define MM_OBJECT_BASE_H
 
 #include "MM_Manager.h"
-
 extern MM_Manager Manager;
 
 template<typename T>
 class MM_Object_Base
 {
 	public:
-		MM_Object_Base(const T& value)
+		MM_Object_Base(const char* name, const T& value)
 		: _value(value)
+		, _name(name)
 		{}
 		
-		operator const T& () const { return value; } // Return value when object name is used
+		// Conversion Operator
+		// Second const is a promise to not modify MM_Object_Base
+		// \return [const T&], returns const ref to _value to prevent caller from modifying
+		operator const T& () const { return _value; }
 
-		// write to value when assigning to instance
+		// Assignment Operator
+		// \param [T&] value, the new value to assign to base
+		// \return [const T&], returns const ref to value instead of copy
 		const T& operator = (const T& value)
 		{
 			_value = value;
@@ -24,6 +29,7 @@ class MM_Object_Base
 
 	private:
 		T _value; // RAM value of the  Object
+		const char* _name; // the name of the object as will appear in MM app
 };
 
 #endif
